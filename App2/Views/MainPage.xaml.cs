c99fbb25119496ea709cb617546102a7d72792c3
jpb <jakub.baranski.33@gmail.com>
2023-06-28 21:30:47 +0100

    Add Axela response UI and start implementing chatbot commands once present in Python version

--- App2/Views/MainPage.xaml.cs
+++ App2/Views/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using App2.ViewModels;
 
 using Microsoft.UI.Xaml.Controls;
+using Windows.System;
 
 namespace App2.Views;
 
@@ -17,11 +18,35 @@ public sealed partial class MainPage : Page
     {
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+
     }
 
     private void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
     {
         AxelaText = axelabox.Text;
         tester.Text = AxelaText;
+        processText();
+    }
+
+    private async void processText()
+    {
+        if (AxelaText != null)
+        {
+            if (AxelaText.Length > 0)
+            {
+                if (AxelaText.Contains("hello")) {
+                    string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+                    AxelaResponseText.Text = "Hello, " + userName;
+                }
+            }
+        }
+    }
+
+    public async void UserInit()
+    {
+        string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+
+
+        text1.Text = "Welcome, " + userName;
     }
 }
