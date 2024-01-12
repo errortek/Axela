4561af927a58ccf98c60abd9bb7590b7b6642765
ErrorTek <jakub.baranski.33@gmail.com>
2024-01-12 22:22:52 +0000

    Update UI, clean up deprecated code

--- App2/Views/MainPage.xaml.cs
+++ App2/Views/MainPage.xaml.cs
@@ -30,9 +30,7 @@ public sealed partial class MainPage : Microsoft.UI.Xaml.Controls.Page
     private async void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
     {
         AxelaText = axelabox.Text;
-        tester.Text = AxelaText;
         AxelaText = AxelaText.ToLower();
-        //ProcessAnswer();
         await ProcessSmartAnswer();
     }
 
@@ -111,87 +109,6 @@ public sealed partial class MainPage : Microsoft.UI.Xaml.Controls.Page
         axelabox.Text = string.Empty;
     }
 
-    private async void ProcessAnswer()
-    {
-        if (AxelaText != null)
-        {
-            if (AxelaText.Length > 0)
-            {
-                if (AxelaText.Contains("hello") || AxelaText.Contains("hi") || AxelaText.Contains("hii")) {
-                    string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
-                    AxelaResponseText.Text = "Hello, " + userName;
-                }
-                else if (AxelaText.Contains("who is your developer"))
-                {
-                    AxelaResponseText.Text = "My developer is jpb, sometimes referred to as jpbandroid :D";
-                }
-                else if (AxelaText.Contains("bye"))
-                {
-                    string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
-                    AxelaResponseText.Text = "Bye! :D\nHave a nice day, " + userName;
-                }
-                else if (AxelaText.Contains("it's my birthday"))
-                {
-                    string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
-                    AxelaResponseText.Text = "Happy birthday, " + userName;
-                }
-                else if (AxelaText.Contains("what's the time"))
-                {
-                    AxelaResponseText.Text = "The time right now is: " + DateTime.Now.ToString();
-                }
-                else if (AxelaText.Contains("get")) {
-                    if (AxelaText.Contains("from wikipedia"))
-                    {
-                        //string WikiArticle = string.Empty;
-                        //AxelaText = WikiArticle;
-                        //WikiArticle.Replace("get ", "");
-                        //WikiArticle.Replace("from wikipedia", "");
-                        //WebClient client = new WebClient();
-
-                        //using (Stream stream = client.OpenRead("http://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext=1&titles=stack%20overflow"))
-                        //using (StreamReader reader = new StreamReader(stream))
-                        //{
-                        //    JsonSerializer ser = new JsonSerializer();
-                        //    Result result = ser.Deserialize<Result>(new JsonTextReader(reader));
-
-                        //    foreach (Page page in result.query.pages.Values)
-                        //        Console.WriteLine(page.extract);
-
-                        //}
-
-                        // This is the path for the "LocalState" folder.
-                        string directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-                        Process P = new();
-                        P.StartInfo.UseShellExecute = true;
-                        P.StartInfo.Verb = "runas";
-                        P.StartInfo.FileName = $@"{directory}\axela_wiki.exe";
-                        P.StartInfo.Arguments = "";
-                        P.Start();
-                        AxelaResponseText.Text = "Please input any queries from Wikipedia into the textbox of the new app window";
-
-                    }
-                }
-                else if (AxelaText.Contains("how are you"))
-                {
-                    AxelaResponseText.Text = "I'm great! :D\nBut I don't really have feelings, as I am an AI chatbot";
-                }
-                else if (AxelaText.Contains("axela, axela"))
-                {
-                    AxelaResponseText.Text = "Hey there, I see you're repeating my name...";
-                }
-                else if (AxelaText.Contains("cortana"))
-                {
-                    AxelaResponseText.Text = "...\ncoming soon\n...";
-                }
-                else
-                {
-                    AxelaResponseText.Text = "Sorry, I couldn't find any response to your command! :(\nMaybe update the app to its latest version and then try again?";
-                }
-            }
-        }
-    }
-
     public async void UserInit()
     {
         string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
@@ -229,9 +146,7 @@ public sealed partial class MainPage : Microsoft.UI.Xaml.Controls.Page
         if (e.Key == VirtualKey.Enter)
         {
             AxelaText = axelabox.Text;
-            tester.Text = AxelaText;
             AxelaText = AxelaText.ToLower();
-            //ProcessAnswer();
             await ProcessSmartAnswer();
         }
     }
