05ea584f16ff4fea77eb2a473b894fe6528f8166
ErrorTek <jakub.baranski.33@gmail.com>
2024-01-12 22:08:00 +0000

    Mica Alt

--- App2/MainWindow.xaml.cs
+++ App2/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using App2.Helpers;
-
+using Microsoft.UI;
+using Microsoft.UI.Composition.SystemBackdrops;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml.Media;
+using Windows.UI;
 using Windows.UI.ViewManagement;
-
 namespace App2;
 
 public sealed partial class MainWindow : WindowEx
@@ -17,6 +20,30 @@ public sealed partial class MainWindow : WindowEx
         AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
         Content = null;
         Title = "AppDisplayName".GetLocalized();
+
+        SystemBackdrop = new MicaBackdrop()
+        {
+            Kind = MicaKind.BaseAlt
+        };
+        var appWindow = AppWindow;
+        var titleBar = appWindow.TitleBar;
+        titleBar.ExtendsContentIntoTitleBar = true;
+        bool isTallTitleBar = true;
+        if (AppWindowTitleBar.IsCustomizationSupported() && appWindow.TitleBar.ExtendsContentIntoTitleBar)
+        {
+            AppWindow.TitleBar.ButtonBackgroundColor = Colors.Transparent;
+            AppWindow.TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            AppWindow.TitleBar.ButtonHoverBackgroundColor = Color.FromArgb(25, 255, 255, 255);
+            AppWindow.TitleBar.ButtonPressedBackgroundColor = Color.FromArgb(25, 200, 200, 200);
+            if (isTallTitleBar)
+            {
+                appWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Tall;
+            }
+            else
+            {
+                appWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Standard;
+            }
+        }
 
         // Theme change code picked from https://github.com/microsoft/WinUI-Gallery/pull/1239
         dispatcherQueue = Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread();
